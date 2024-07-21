f0981b75a77cea7a6c377fef91937d8c160132d3
Mohit Sapat <103207988+sapatmohit@users.noreply.github.com>
2024-07-21 23:00:43 +0530

    Our teams sections and About Us section added

--- js/script.js
+++ js/script.js
@@ -27,3 +27,11 @@ document.addEventListener('DOMContentLoaded', function () {
 		},
 	});
 });
+
+window.addEventListener('scroll', function () {
+	if (this.pageYOffset > 60) {
+		document.querySelector('.navbar').classList.add('sticky');
+	} else {
+		document.querySelector('.navbar').classList.remove('sticky');
+	}
+});
