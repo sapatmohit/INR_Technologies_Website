ebaaca3e580fd7d58f2025485cc286c1909cfd87
Mohit Sapat <103207988+sapatmohit@users.noreply.github.com>
2024-08-06 22:19:02 +0530

    added 320px responsive support

--- js/script.js
+++ js/script.js
@@ -70,3 +70,20 @@ window.addEventListener('scroll', function () {
 		}
 	});
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+	const hamburger = document.querySelector('#hamburger');
+	const navMenu = document.querySelector('.nav');
+
+	hamburger.addEventListener('click', () => {
+		hamburger.classList.toggle('active');
+		navMenu.classList.toggle('active');
+	});
+
+	document.querySelectorAll('.nav-items').forEach((n) =>
+		n.addEventListener('click', () => {
+			hamburger.classList.remove('active');
+			navMenu.classList.remove('active');
+		})
+	);
+});
